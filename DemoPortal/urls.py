8d77bc6feac61267d03a071be8c2b7f7cb998313
MrPoliteGrizly <lbekework@gmail.com>
2018-07-02 23:11:27 +0600

    Made a header

--- DemoPortal/urls.py
+++ DemoPortal/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from Login.views import login, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^login/', login, name='login'),
+    url(r'^home/', home, name='home'),
 ]
